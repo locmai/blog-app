1464a1619f409dce4e15ce23edaa7e8c353346bb
vandt <vanthihongnguyen@kms-technology.com>
2020-04-30 17:51:44 +0700

    Fix ssr style

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,45 +1,54 @@
-import { HeaderMenu } from '@components/Menu';
-import { Layout, Row, Col } from 'antd';
-import Head from 'next/head';
-import { FC } from 'react';
+import { HeaderMenu } from "@components/Menu";
+import { Layout, Row, Col } from "antd";
+import Head from "next/head";
+import { FC } from "react";
 import styled from "styled-components";
 
 type Props = {
-    title?: string;
-    page?: string;
+  title?: string;
+  page?: string;
 };
 
 const { Header, Content, Footer } = Layout;
 
-export const CustomLayout = styled(Layout) <Props>`
-    .ant-layout-header {
-        background: white;
-        line-height: 40px;
-    }
-`
+export const CustomLayout = styled(Layout)`
+  .ant-layout-header {
+    background: white;
+    line-height: 40px;
+  }
+`;
 
+const CustomHeader = styled(Header)`
+  background: white;
+  line-height: 40px;
+`;
 const MasterLayout: FC<Props> = ({
-    children,
-    title = "LM blog",
-    page = "home",
+  children,
+  title = "LM blog",
+  page = "home",
 }) => (
-        <CustomLayout id="page-container" className="master-layout">
-            <Head>
-                <title>{title}</title>
-            </Head>
+  <Layout id="page-container" className="master-layout">
+    <Head>
+      <title>{title}</title>
+    </Head>
 
-            <Header style={{ justifyContent: 'center', display: 'flex' }}>
-                <HeaderMenu page={page}></HeaderMenu>
-            </Header>
-            <Row gutter={[8, 16]} align="middle">
-                <Col xs={{ span: 20, offset: 2 }} xl={{ span: 16, offset: 4 }}>
-                    <Content id="content-wrap" style={{ justifyContent: 'center', display: 'flex' }}>
-                        <div>{children}</div>
-                    </Content>
-                </Col>
-            </Row>
-            <Footer id="footer" style={{ textAlign: 'center' }}>Loc Mai ©2020</Footer>
-        </CustomLayout>
-    );
+    <CustomHeader style={{ justifyContent: "center", display: "flex" }}>
+      <HeaderMenu page={page}></HeaderMenu>
+    </CustomHeader>
+    <Row gutter={[8, 16]} align="middle">
+      <Col xs={{ span: 20, offset: 2 }} xl={{ span: 16, offset: 4 }}>
+        <Content
+          id="content-wrap"
+          style={{ justifyContent: "center", display: "flex" }}
+        >
+          {children}
+        </Content>
+      </Col>
+    </Row>
+    <Footer id="footer" style={{ textAlign: "center" }}>
+      Loc Mai ©2020
+    </Footer>
+  </Layout>
+);
 
-export default MasterLayout
+export default MasterLayout;
--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,35 +1,45 @@
-import { Menu, Space } from 'antd';
-import { FC } from 'react';
+import { Menu, Space } from "antd";
+import { FC } from "react";
 import styled from "styled-components";
+import Link from "next/link";
 
 type Props = {
-    page?: string;
+  page?: string;
 };
 
-const CustomMenu  = styled(Menu) <Props>`
-    .ant-menu-item {
-        padding: 40px 0px 40px 0px;
-    }
-`
+const CustomMenu = styled(Menu)<Props>`
+  .ant-menu-item {
+    padding: 40px 0px 40px 0px;
+  }
+`;
 
 const BaseMenu: FC<Props> = ({ page }) => {
-
-    return (
-        <Space size={'large'}>
-            <Menu defaultSelectedKeys={[page]} mode="horizontal">
-                <Menu.Item key="home"><a href="/">
-                    Home</a></Menu.Item>
-                <Menu.Item key="posts"><a href="/posts">
-                    Posts</a></Menu.Item>
-                <Menu.Item key="projects"><a href="/projects">
-                    Projects</a></Menu.Item>
-                <Menu.Item key="about"><a href="/about">
-                    About</a></Menu.Item>
-            </Menu>
-        </Space>
-    );
+  return (
+    <Space size={"large"}>
+      <Menu defaultSelectedKeys={[page]} mode="horizontal">
+        <Menu.Item key="home">
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="posts">
+          <Link href="/posts">
+            <a>Posts</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="projects">
+          <Link href="/projects">
+            <a>Projects</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="about">
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </Menu.Item>
+      </Menu>
+    </Space>
+  );
 };
 
-
-export const HeaderMenu = styled(BaseMenu) <Props>`
-`
+export const HeaderMenu = styled(BaseMenu)<Props>``;
